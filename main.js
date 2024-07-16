cb4d5665b128b1c06bc9a573be251eb27d599771
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-16 18:03:48 +0200

    [UPD] active-buttons

--- main.js
+++ main.js
@@ -61,36 +61,41 @@ const container = document.getElementById('container');
 //CICLO GLI ELEMENTI PER INSERIRLI NEL CONTAINER//
 
 posts.forEach((post) => {
+
+    let { id, author, content, media, likes, created } = post;
     container.innerHTML += `
               <div class="post">
             <div class="post__header">
                 <div class="post-meta">                    
                     <div class="post-meta__icon">
-                        <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">                    
+                        <img class="profile-pic" src="${author.image}" alt="${author.name}">                    
                     </div>
                     <div class="post-meta__data">
-                        <div class="post-meta__author">${post.author.name}</div>
-                        <div class="post-meta__time">4 mesi fa</div>
+                        <div class="post-meta__author">${author.name}</div>
+                        <div class="post-meta__time">${created}</div>
                     </div>                    
                 </div>
             </div>
-            <div class="post__text">${post.content}</div>
+            <div class="post__text">${content}</div>
             <div class="post__image">
-                <img src="${post.media}" alt="">
+                <img src="${media}" alt="">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <a class="like-button  js-like-button" href="#" data-postid="${id}">
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
                     </div>
                 </div> 
             </div>            
         </div> `;
 });
 
+//RECUPERO IL PULSANTE DAL DOM//
+const like_buttons = document.querySelectorAll('.js-likes-buttons');
+console.log(like_buttons);
